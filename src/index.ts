bb5be186b7174d24d2934d318a6b336c6a4f0dec
marksikaundi <emmanuelmark117@gmail.com>
2023-12-23 07:29:37 +0200

    Add dependencies and update server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World");
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    // origin: 'http://localhost:3000',
+    credentials: true
+    }));
+
+app.use(cookieParser());
+app.use(compression());
+app.use(bodyParser.json());
+
+
+const server = http.createServer(app);
+
+server.listen(8080, () =>{
+    console.log('server is running on http://localhost:8080');
+})
+
